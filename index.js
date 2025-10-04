1ab120905d6e4ba4b8864d6c1dba3fb3cd3411a0
Yil2000 <yannai.iluz@gmail.com>
2025-10-05 02:13:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // ===== Clean old JWT =====
   // רק בדף login.html
-  sessiontorage.removeItem("token");
+  sessionstorage.removeItem("token");
 
   // ===== Admin Login Button =====
   const adminLoginBtn = document.querySelector(".nav-login-btn");
@@ -210,4 +210,5 @@ document.addEventListener("DOMContentLoaded", () => {
   setupSliding(".special-activity-content-sliding-img");
 
 });
+
 
