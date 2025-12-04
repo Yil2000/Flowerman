472a41e0fc1eaf62c5ac64c0c189b7f0d5094ece
Yil2000 <yannai.iluz@gmail.com>
2025-12-04 23:55:55 +0200

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,19 +1,27 @@
 import NodeCache from "node-cache";
 
-const cache = new NodeCache({ stdTTL: 600 });
+const cache = new NodeCache({ stdTTL: 86400 }); // 24 שעות
 
 export function cacheMiddleware(req, res, next) {
   const key = req.originalUrl;
   const cached = cache.get(key);
 
   if (cached) {
-    return res.send(cached);
+    if (cached.isJson) return res.json(cached.data);
+    return res.send(cached.data);
   }
 
   const originalSend = res.send.bind(res);
+  const originalJson = res.json.bind(res);
+
   res.send = (body) => {
-    cache.set(key, body);
-    originalSend(body);
+    cache.set(key, { data: body, isJson: false });
+    return originalSend(body);
+  };
+
+  res.json = (data) => {
+    cache.set(key, { data, isJson: true });
+    return originalJson(data);
   };
 
   next();
